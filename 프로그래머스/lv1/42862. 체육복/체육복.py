446814fcff2d9d1855140bdc410da3eeef21e8c0
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-10-03 17:06:19 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,21 +1,20 @@
+
 def solution(n, lost, reserve):
     lost_copy = [x for x in lost if x not in reserve]
     reserve_copy = [x for x in reserve if x not in lost]
     lost_copy.sort()
-    reserve_copy.sort()
-
     answer = n - len(lost_copy)
-    for index in lost_copy:
-        if index in reserve_copy:
-            answer += 1
-            reserve_copy.remove(index)
-
-        elif index - 1 in reserve_copy:
-            answer += 1
-            reserve_copy.remove(index - 1)
-
-        elif index + 1 in reserve_copy:
-            answer += 1
-            reserve_copy.remove(index + 1)
-
+    for each in lost_copy:
+        if each - 1 in reserve_copy:
+          reserve_copy.remove(each - 1)
+          answer += 1
+        elif each + 1 in reserve_copy:
+          reserve_copy.remove(each + 1)
+          answer += 1
     return answer
+
+def main():
+  print(solution(3, [3], [1]))
+
+if __name__ == "__main__":
+    main()
