e34b74ff12bc8379ce9681d309ddb8036db39824
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-10-07 18:13:05 +0900

    [level 2] Title: 구명보트, Time: 8.38 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,26 +1,13 @@
 def solution(people, limit):
-
-    # 몸무게를 오름차순 정렬
-    people.sort()
-
-    # 시작, 끝 인덱스
-    start, end = 0, len(people)-1
-
-    # 보트의 수
-    count = 0
-
-    while start <= end:
-
-        # 보트의 수 증가
-        count += 1
-
-        # 두 명(몸무게가 가장 적은 사람, 몸무게가 가장 많은 사람)을 태우거나
-        if (people[start] + people[end] <= limit):
-            start += 1
-            end -= 1
-
-        # 한 명(몸무게가 가장 많은 사람)을 태웁니다.   
-        else:
-            end -= 1
-
-    return count
+  people.sort()
+  left_index = 0
+  right_index = len(people) - 1
+  count = len(people)
+  while left_index < right_index:
+    if people[left_index] + people[right_index] <= limit:
+      left_index += 1
+      right_index -= 1
+      count -= 1
+      continue
+    right_index -= 1 
+  return count
