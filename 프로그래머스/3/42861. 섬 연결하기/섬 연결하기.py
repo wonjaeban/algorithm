188d59a7b1e8507476f360c9732cfea4f306ae11
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-10-15 17:34:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,16 +1,13 @@
 def solution(n, costs):
-    answer = 0
-    costs.sort(key = lambda x: x[2]) 
-    link = set([costs[0][0]])
-
-    # Kruskal 알고리즘으로 최소 비용 구하기
-    while len(link) != n:
-        for v in costs:
-            if v[0] in link and v[1] in link:
-                continue
-            if v[0] in link or v[1] in link:
-                link.update([v[0], v[1]])
-                answer += v[2]
-                break
-                
-    return answer
+  costs.sort(key = lambda x: x[2])
+  list = set([costs[0][0]])
+  answer = 0
+  while len(list) != n:
+    for element in costs:
+      if element[0] in list and element[1] in list:
+        continue
+      elif element[0] in list or element[1] in list:
+        list.update([element[0], element[1]])
+        answer += element[2]
+        break
+  return answer
