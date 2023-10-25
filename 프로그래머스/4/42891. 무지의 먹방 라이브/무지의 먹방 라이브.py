9c0c32ebc16c0c66e0a0683d4b86165d9054ad80
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-10-25 22:56:53 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 169.81 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -1,29 +1,19 @@
-# 무지의 먹방 라이브
 import heapq
 
-
 def solution(food_times, k):
-    answer = -1
-    h = []
-    for i in range(len(food_times)):
-        heapq.heappush(h, (food_times[i], i + 1))
+  heap = []
+  for idx, food in enumerate(food_times):
+    heapq.heappush(heap, (food, idx+1))
 
-    food_num = len(food_times)  # 남은 음식 개수
-    previous = 0  # 이전에 제거한 음식의 food_time
+  prev_sum = 0
 
-    while h:
-        # 먹는데 걸리는 시간: (남은 양) * (남은 음식 개수)
-        t = (h[0][0] - previous) * food_num
-        # 시간이 남을 경우 현재 음식 빼기
-        if k >= t:
-            k -= t
-            previous, _ = heapq.heappop(h)
-            food_num -= 1
-        # 시간이 부족할 경우(음식을 다 못먹을 경우) 남은 음식 중에 먹어야 할 음식 찾기
-        else:
-            idx = k % food_num
-            h.sort(key=lambda x: x[1])
-            answer = h[idx][1]
-            break
-
-    return answer
+  while heap:
+    food = heap[0][0]
+    if (food - prev_sum) * len(heap) <= k:
+      k -= ((food - prev_sum) * len(heap))
+      heapq.heappop(heap)
+      prev_sum = food
+    else:
+      heap.sort(key=lambda x: x[1])
+      return heap[k % len(heap)][1]
+  return -1
