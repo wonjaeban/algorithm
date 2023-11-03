1b6685a2e9227fedc31ac1a0206dfd6a8d141bd5
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-11-03 22:58:34 +0900

    [level 1] Title: 최소직사각형, Time: 4.37 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,11 @@
 def solution(sizes):
-    max_list=[]
-    min_list=[]
+    largest = 0
+    other_size = 0
     for size in sizes:
-        max_list.append(max(size))
-        min_list.append(min(size))
-    return max(max_list) * max(min_list)
+        if largest < max(size):
+            largest = max(size)
+        if other_size < min(size):
+            other_size = min(size)
+    
+    answer = largest * other_size
+    return answer
