f545bb76b219f45d411903772db12942f573b417
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-11-11 15:22:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.93 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,34 +1,21 @@
-# 너비 우선 탐색
 from collections import deque
 
-def solution(maps):
-    if len(maps) == 1 and len(maps[0]) == 1:
-        return 1
-    row_len = len(maps)
-    vec_len = len(maps[0])
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-
-    # 맵 받기
-    def bfs():
-        queue = deque()
-        queue.append((0, 0))
+def bfs(maps):
+    queue = deque([[0, 0]])
+    dx = [+1, -1, 0, 0]
+    dy = [0, 0, +1, -1]
+    
+    while queue:
+        row_idx, col_idx = queue.popleft()
+        for i in range(4):
+            new_row = row_idx + dx[i]
+            new_col = col_idx + dy[i]
+            if 0 <= new_row < len(maps) and 0 <= new_col < len(maps[0]) and maps[new_row][new_col] == 1:
+                queue.append([new_row, new_col])
+                maps[new_row][new_col] = maps[row_idx][col_idx] + 1
+                
+    return maps[len(maps) - 1][len(maps[0]) - 1] if maps[len(maps) - 1][len(maps[0]) - 1] > 1 else -1
         
-        while queue:
-            row, vec = queue.popleft()
-            if (row, vec) == (row_len - 1, vec_len - 1):
-                return maps[row][vec]
-            for (y, x) in zip(dy, dx):
-                new_row = row + y
-                new_vec = vec + x
-                if new_row < 0 or new_row >= row_len or \
-                    new_vec < 0 or new_vec >= vec_len or \
-                    maps[new_row][new_vec] == 0 or \
-                    (maps[new_row][new_vec] != 1 and 
-                     maps[new_row][new_vec] <= maps[row][vec] + 1):
-                    continue
-                maps[new_row][new_vec] = maps[row][vec] + 1
-                queue.append((new_row, new_vec))
-        return -1
 
-    return bfs()
+def solution(maps):
+    return bfs(maps)
