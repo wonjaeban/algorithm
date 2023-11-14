525e5dd25068450785eaf2319f070d245e160399
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-11-14 23:24:19 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,35 +1,24 @@
 def solution(tickets):
-    answer = []
+    tickets.sort(key=lambda x: (x[0], x[1]))
     
-    # 출발지가 같은 티켓끼리 쭉 나열되도록 출발지 기준 정렬
-    # 같은 출발지에 대해서 도착지 기준 정렬(알파벳 순서 상 앞에꺼 먼저 방문하기 위함)
-    tickets.sort(key = lambda x: (x[0], x[1]))
-    
-    # DFS
-    def getPath(t, path):
-        # 티켓을 모두 소진했다면 현재까지의 path 그대로 리턴
-        if len(t) == 0:
+    def dfs(remain_t, path):
+        if len(remain_t) == 0:
             return path
-        
-        now = path[-1]
+        cur = path[-1]
         valid_idx = -1
         
         # 남은 티켓(정렬된 상태) 중에서, 출발지가 현재 공항인 티켓의 인덱스를 찾음
         # 조건을 만족하는 티켓 중 가장 왼쪽의 티켓에서 멈춤
-        for i in range(len(t)):
-            if t[i][0] == now:
+        for i in range(len(remain_t)):
+            if remain_t[i][0] == cur:
                 valid_idx = i
                 break
-
-        # 출발지가 현재 공항인 티켓을 모두 순회하면서 DFS 돌림
-        # 하나라도 먼저 완성된 루트가 나온다면 그걸 리턴해줌
-        # 같은 출발지 기준, 도착지를 기준으로 오름차순 정렬했었기 때문에
-        # 먼저 완성된 루트가 나온다면 그게 곧 알파벳 순서 상 가장 앞선 루트가 됨
-        while t[valid_idx][0] == now:
-            nxt_path = getPath(t[:valid_idx] + t[valid_idx + 1:], path + [t[valid_idx][1]])
-            if nxt_path != []:
-                return nxt_path
-            valid_idx += 1
+        
+        while remain_t[valid_idx][0] == cur:
+                next = dfs(remain_t[:valid_idx] + remain_t[valid_idx + 1:], path + [remain_t[valid_idx][1]])
+                if next != []:
+                    return next
+                valid_idx += 1
         return []
-    
-    return getPath(tickets, ["ICN"])
+    return dfs(tickets, ["ICN"])
+     
