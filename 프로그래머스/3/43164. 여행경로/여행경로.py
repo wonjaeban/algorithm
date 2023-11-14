b9adde07614eadcfddac111b61e0f8838ee03ed6
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2023-11-14 23:30:18 +0900

    Update 여행경로.py

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -5,20 +5,11 @@ def solution(tickets):
         if len(remain_t) == 0:
             return path
         cur = path[-1]
-        valid_idx = -1
         
-        # 남은 티켓(정렬된 상태) 중에서, 출발지가 현재 공항인 티켓의 인덱스를 찾음
-        # 조건을 만족하는 티켓 중 가장 왼쪽의 티켓에서 멈춤
-        for i in range(len(remain_t)):
-            if remain_t[i][0] == cur:
-                valid_idx = i
-                break
-        
-        while remain_t[valid_idx][0] == cur:
-                next = dfs(remain_t[:valid_idx] + remain_t[valid_idx + 1:], path + [remain_t[valid_idx][1]])
+        for idx, remain in enumerate(remain_t):
+            if remain[0] == cur:
+                next = dfs(remain_t[:idx] + remain_t[idx + 1:], path + [remain[1]])
                 if next != []:
                     return next
-                valid_idx += 1
         return []
     return dfs(tickets, ["ICN"])
-     
