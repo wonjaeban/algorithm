7e1a11b6168ed87a82e898576f7a32b872aae27d
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2024-01-19 23:13:22 +0900

    [Silver I] Title: 스티커, Time: 648 ms, Memory: 48632 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,17 +1,30 @@
 import sys
-input = sys.stdin.readline
 
-T = int(input())
 
-for _ in range(T):
-    n = int(input())
-    dp = [list(map(int,input().split())) for _ in range(2)]
 
+def solution():
+  T = int(sys.stdin.readline())
+  answer = []
+  for _ in range(T):
+    n = int(sys.stdin.readline())
+    inputs = []
+    inputs.append(list(map(int, sys.stdin.readline().split()))) 
+    inputs.append(list(map(int, sys.stdin.readline().split()))) 
+
+    dp = [[0 for _ in range(n)] for _ in range(2)]
+    
+    dp[0][0] = inputs[0][0]
+    dp[1][0] = inputs[1][0]
     if n > 1:
-        dp[0][1] += dp[1][0]
-        dp[1][1] += dp[0][0]
-    for i in range(2,n):
-        dp[0][i] += max(dp[1][i-1],dp[1][i-2])
-        dp[1][i] += max(dp[0][i-1],dp[0][i-2])
+      dp[0][1] = inputs[0][1] + inputs[1][0]
+      dp[1][1] = inputs[1][1] + inputs[0][0]
+    
+    for i in range(2, n):
+      dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + inputs[0][i]
+      dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + inputs[1][i]
+    answer.append(max(max(dp[0]), max(dp[1])))
 
-    print(max(dp[0][n-1],dp[1][n-1]))
+  for elemet in answer:
+    print(elemet)
+    
+solution()
