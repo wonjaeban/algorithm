1347d08b0889b1b6b2f2c2e71b1b9e0f83c75495
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2024-01-27 23:57:07 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 772 ms, Memory: 109812 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,19 +1,25 @@
-import sys 
-input = sys.stdin.readline
+import sys
 
-n, m = map(int,input().split())
+def solution():
+  N, M = map(int, sys.stdin.readline().split())
+  answer = []
+  graph = []
+  for _ in range(N):
+    graph.append(list(map(int, sys.stdin.readline().split())))
 
-array = []
-for _ in range(n):
-    array.append(list(map(int,input().split())))
+  dp = [[0] * (N + 1) for _ in range(N + 1)]
 
-sum_dp = [[0] * (n + 1) for _ in range(n + 1)]
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        sum_dp[i][j] = sum_dp[i][j-1] + sum_dp[i-1][j] - sum_dp[i-1][j-1] + array[i-1][j-1]
+  for x in range(1, N + 1):
+    for y in range(1, N + 1):
+      dp[x][y] = dp[x-1][y] + dp[x][y-1] - dp[x-1][y-1] + graph[x-1][y-1]
+    
+  for _ in range(M):
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    sum = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]
+    answer.append(sum)
 
-for _ in range(m):
-    x1, y1, x2, y2 = map(int,input().split())
-
-    ans = sum_dp[x2][y2] - sum_dp[x2][y1-1] - sum_dp[x1-1][y2] + sum_dp[x1-1][y1-1]
+  for ans in answer:
     print(ans)
+    
+    
+solution()
