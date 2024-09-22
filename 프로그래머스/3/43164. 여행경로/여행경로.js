e9bee35e41bef4fd45ad3c6d06f40e58e2e0b1f2
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2024-09-22 20:41:42 +0900

    [level 3] Title: 여행경로, Time: 500.75 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,31 +1,24 @@
 function solution(tickets) {
-    const graph = new Map();
-    const result = [];
+  const answer = [];
+  const len = tickets.length;
+	const visited = Array(len).fill(false);
 
-    // 그래프 구성
-    for (const [start, end] of tickets) {
-        if (graph.has(start)) {
-            graph.get(start).push(end);
-            graph.get(start).sort();  // 알파벳 순으로 정렬
-        } else {
-            graph.set(start, [end]);
+	const dfs = (routes) => {
+    if (routes.length === len + 1) answer.push(routes);
+
+    for (const idx in tickets) {
+      const [start, end] = tickets[idx];
+      if (!visited[idx]) {
+        if (routes.at(-1) === start) {
+          visited[idx] = true;
+          dfs([...routes, end]);
+          visited[idx] = false;
         }
+      }
     }
+  };
 
-    function DFS(cur) {
-        const possible = graph.get(cur) || [];
+  dfs(["ICN"]);
 
-        // 가능한 모든 경로를 탐색
-        while (possible.length > 0) {
-            const next = possible.shift();  // 알파벳 순으로 가장 앞의 경로 선택
-            DFS(next);
-        }
-
-        // 경로가 끝나면 결과에 추가 (역순으로 추가)
-        result.push(cur);
-    }
-
-    DFS('ICN');  // 출발지 'ICN'에서 시작
-
-    return result.reverse();  // 결과를 역순으로 반환
+  return answer.sort()[0];
 }
