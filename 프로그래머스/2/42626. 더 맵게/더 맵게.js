76c975ff931e66923a89a17e1b5b285d5e5a607f
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2024-10-05 21:08:37 +0900

    [level 2] Title: 더 맵게, Time: 377.07 ms, Memory: 99.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,73 @@
+// 해당 문제는 Heap 구조를 활용해야 함
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+      
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+
+    while (
+      currentIndex > 0 &&
+      this.heap[currentIndex] < this.heap[Math.floor((currentIndex - 1) / 2)]
+    ) {
+      const temp = this.heap[currentIndex];
+      this.heap[currentIndex] = this.heap[Math.floor((currentIndex - 1) / 2)];
+      this.heap[Math.floor((currentIndex - 1) / 2)] = temp;
+      currentIndex = Math.floor((currentIndex - 1) / 2);
+    }
+  }
+
+  pop() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const minValue = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    let currentIndex = 0;
+
+    while (currentIndex * 2 + 1 < this.heap.length) {
+      let minChildIndex = currentIndex * 2 + 2 < this.heap.length && this.heap[currentIndex * 2 + 2] < this.heap[currentIndex * 2 + 1] ? currentIndex * 2 + 2 : currentIndex * 2 + 1;
+
+      if (this.heap[currentIndex] < this.heap[minChildIndex]) {
+        break;
+      }
+
+      const temp = this.heap[currentIndex];
+      this.heap[currentIndex] = this.heap[minChildIndex];
+      this.heap[minChildIndex] = temp;
+      currentIndex = minChildIndex;
+    }
+
+    return minValue;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+}
+
+function solution(scoville, K) {
+  const minHeap = new MinHeap();
+
+  for (const ele of scoville) {
+    minHeap.push(ele);
+  }
+
+  let answer = 0;
+
+  while (minHeap.size() >= 2 && minHeap.peek() < K) {
+    const first = minHeap.pop();
+    const second = minHeap.pop();
+    const mixedScov = first + second * 2;
+    minHeap.push(mixedScov);
+    answer++;
+  }
+
+  return minHeap.peek() >= K ? answer : -1;
+}
