65ff4e0261ecf8fb17299006650ebc38edefee76
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2024-10-21 23:11:27 +0900

    [level 3] Title: 가장 먼 노드, Time: 1146.76 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,24 +1,32 @@
-function solution (n, edge) {
-  const connects = new Array(n).fill().map(_ => []);
-  for(const e of edge) {
-    connects[e[0]-1].push(e[1]-1);
-    connects[e[1]-1].push(e[0]-1);
-  }
-  
-  const visited = [1];
-  const queue = [0];
-  while(queue.length) {
-    const cur = queue.shift();
-    
-    for(const next of connects[cur]) {
-      if(!visited[next]) {
-        visited[next] = visited[cur] + 1;
-        queue.push(next);
-      }
+function solution(n, edge) {
+    const graph = Array.from({length: n + 1}, ()=> []);
+    for (ed of edge) {
+        graph[ed[0]].push(ed[1]);
+        graph[ed[1]].push(ed[0]);
     }
-  }
-  
-  const max = Math.max(...visited);
-  
-  return visited.filter(el => el === max).length;
+    const visited = [1];
+    const queue = [1];
+    const counts = Array.from({length: n + 1}, () => 0);
+    
+    function BFS () {
+        while (queue.length > 0) {
+            const cur = queue.shift();
+            for (next of graph[cur]) {
+                if (!visited.includes(next)) {
+                    queue.push(next);
+                    visited.push(next);
+                    counts[next] = counts[cur] + 1;
+                }
+            }
+        }  
+    }
+    BFS();
+    const max = Math.max(...counts);
+    let answer = 0;
+    for (count of counts) {
+        if (count === max) {
+            answer += 1;
+        }
+    }
+    return answer;
 }
