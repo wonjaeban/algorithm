0476c23424c673cf8cd829d913c4f4c931f53ca2
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2024-11-04 21:46:34 +0900

    [level 3] Title: 순위, Time: 16.30 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,38 +1,38 @@
 function solution(n, results) {
     const graph = Array.from({length: n + 1}, () => Array(n + 1).fill(0));
-    for (result of results) {
+    
+    for (const result of results) {
         graph[result[0]][result[1]] = 1;
         graph[result[1]][result[0]] = -1;
     }
-    for (let mid = 1; mid <= n; mid++) {
-        for (let start = 1; start <= n; start++) {
-            for (let end = 1; end <= n; end++) {
-                if(graph[start][mid] === 1 && graph[mid][end] === 1) {
-                    graph[start][end] = 1
-                } else if(graph[start][mid] === -1 && graph[mid][end] === -1) {
-                    graph[start][end] = -1;
+    
+    for (let j = 1; j < n + 1; j++) {
+        for (let i = 1; i < n + 1; i++) {
+            for (let k = 1; k < n + 1; k++) {
+                if (i === j || j === k) {
+                    continue;
                 }
-            }
+                if (graph[i][j] === 1 && graph[j][k] === 1) {
+                    graph[i][k] = 1;
+                } else if (graph[i][j] === -1 && graph[j][k] === -1) {
+                    graph[i][k] = -1;
+                }
+            }   
         }
     }
-    
     let answer = 0;
-    
-    for (let i = 1; i <= n; i++) {
-        let isDecided = true;
-        for (let j = 1; j <= n; j++) {
-            if (i !== j && graph[i][j] === 0) {
-                isDecided = false;
-                break;
+    for (let i = 1; i < n + 1; i++) {
+        const result = graph[i];
+        let count = 0;
+        for (const element of result) {
+            if (element === 0) {
+                count += 1;
             }
         }
-        if (isDecided) {
+        if (count === 2) {
             answer += 1;
         }
     }
-    //
-    
     
     return answer;
-    
 }
