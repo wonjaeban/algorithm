fdc237e4e8ea5974438cc4cf8d8b94359dbd4861
wonjae Ban <70055619+wonjaeban@users.noreply.github.com>
2024-12-23 21:29:00 +0900

    [Silver III] Title: N과 M (7), Time: 2956 ms, Memory: 595732 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,32 @@
+let fs = require('fs');
+let inputs = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const [N, M] = inputs[0].split(' ').map(Number);
+const candidates = inputs[1].split(' ').map(Number).sort((a, b) => a - b);
+
+let answer = '';
+
+const makeComb = (arr, cnt) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        const fix = arr[i];
+        if(cnt === 1) {
+            result.push([fix]);
+        } else {
+            // const rest = arr.filter((each) => each > arr[i]);
+            const newComb = makeComb(arr, cnt - 1);
+            for (let j = 0; j < newComb.length; j++) {
+                result.push([fix, ...newComb[j]]);
+            }
+        }
+    }
+    return result;
+};
+
+const answers = makeComb(candidates, M);
+
+for (let i = 0; i < answers.length; i++) {
+    answer = answer + answers[i].join(' ') + '\n'
+}
+
+console.log(answer.toString().trim());
